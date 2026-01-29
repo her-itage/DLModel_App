d05e621ace3f2bb302baa5f9ce27a1c4375551e8
stephen <plarailsg@gmail.com>
2026-01-29 04:10:21 +0000

    Implemented CI

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,14 +1,3 @@
-import pytest
-from application import app as flask_app
-
-@pytest.fixture
-def app():
-    yield flask_app
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
 def test_index_page(client):
     # test "/" page to verify that the service is online
     res = client.get("/")
